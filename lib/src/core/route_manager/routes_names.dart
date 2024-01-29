de920a15aa875c8b2d8c7b8cf425d9a3fe4b82a8
TRAORE Abdoul-Aziz <112016586+AzizProg@users.noreply.github.com>
2024-01-29 15:15:10 +0000

    Update routes_names.dart

--- lib/src/core/route_manager/routes_names.dart
+++ lib/src/core/route_manager/routes_names.dart
@@ -1,7 +1,7 @@
 class RoutesNames{
   static String login="/";
   static String home="/home";
-  static String transferDetail="transfer_detail";
+  static String transferDetail="/transfer_detail";
   static String bank="/bank";
   static String settings="/settings";
 
